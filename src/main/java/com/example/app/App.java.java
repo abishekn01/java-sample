9e569750732f538ac7c7c110d017fb0fddb99b75
Abishek N <abishekn2001@gmail.com>
2025-09-09 14:31:30 +0530

    Add pom.xml and src folder

---
+++ src/main/java/com/example/app/App.java.java
@@ -0,0 +1,10 @@
+package com.example.app;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        port(8080);
+        get("/hello", (req, res) -> "Hello, CI/CD World!");
+    }
+}
